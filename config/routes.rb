619108ddc849c99d98ae20e78a1f0fdbad409700
Hana Harencarova <h.harencarova@gmail.com>
2023-04-08 12:44:59 +0200

    Add user routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,20 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root 'pages#index'
+
+  resources :users, only: %i[new create]
+  namespace :user do
+    resources :email_confirmations, only: %i[new create]
+    namespace :email_confirmations do
+      post :resend
+    end
+    resources :password_reset_requests, only: %i[new create]
+    resources :password_resets, only: %i[new create]
+  end
+
+  resource :user_session, only: %i[new create destroy]
+
+  get 'log_in', to: redirect('/user_session/new')
+  get 'sign_up', to: redirect('/users/new')
+  delete 'log_out', to: 'user_sessions#destroy'
 end
