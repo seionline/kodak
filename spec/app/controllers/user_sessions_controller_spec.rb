f6d1295b40b8e0ddf57ad68fa4d7c791d816e597
Hana Harencarova <h.harencarova@gmail.com>
2023-04-08 12:48:08 +0200

    WIP specs for users session controller

---
+++ spec/app/controllers/user_sessions_controller_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'UserSessionsController', type: :request do
+  fixtures :users
+
+  it 'should get new' do
+    debug
+    get new_user_session_url
+    assert_response :success
+  end
+
+  # describe 'POST - /u'
+  it 'should log user in successfully' do
+    post user_session_url, params: { user_session: { email: 'user', password: 'user_password' } }
+    assert_equal users(:user).id, session[:current_user_id]
+    assert_redirected_to root_url
+  end
+
+  it 'should fail to log in user' do
+    post user_session_url, params: { user_session: { email: 'user', password: 'wrong_password' } }
+    assert_nil session[:current_user_id]
+    assert_response :unprocessable_entity
+  end
+
+  it 'should log out user' do
+    post user_session_url, params: { user_session: { email: 'user', password: 'user_password' } }
+    assert_redirected_to root_url
+
+    delete user_session_url
+    assert_nil session[:current_user_id]
+    assert_redirected_to root_url
+  end
+end
