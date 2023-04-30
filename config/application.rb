b2a5e6e274dd84d23a711acb8ca8dfa523e2337b
Ferdinand Niedermann <nerdinand@users.noreply.github.com>
2023-04-30 17:20:30 +0200

    Remove css_compressor configuration

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,5 @@ module Rails7App
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
-    config.assets.css_compressor = nil
   end
 end
